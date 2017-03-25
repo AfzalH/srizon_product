9223ce22732a0c4537ed2ad8d5ddf341dce8450f
Afzal Hossain <afzal.csedu@gmail.com>
2017-03-25 18:45:48 +0600

    dropdown support on touch screen clean

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,5 @@
 jQuery(document).ready(function () {
     jQuery('ul#primary-menu > li.menu-item-has-children').click(function (e) {
-        console.log(jQuery(this).find('ul').first().css('opacity'));
         if (jQuery(this).find('ul').first().css('opacity') < 1) {
             e.preventDefault();
         }
