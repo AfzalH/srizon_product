31b5b366bc5f907821795542262ccd23bf8fc359
Afzal Hossain <afzal.csedu@gmail.com>
2017-03-25 18:42:25 +0600

    dropdown support on touch screen

--- theme.js
+++ theme.js
@@ -8268,6 +8268,14 @@ Picker.extend( 'pickadate', DatePicker )
 
 }( jQuery ));
 
+jQuery(document).ready(function () {
+    jQuery('ul#primary-menu > li.menu-item-has-children').click(function (e) {
+        console.log(jQuery(this).find('ul').first().css('opacity'));
+        if (jQuery(this).find('ul').first().css('opacity') < 1) {
+            e.preventDefault();
+        }
+    })
+});
 /**
  * File navigation.js.
  *
