8cb1ee7436f50da55b1ce34efb625bb1078ee8c8
Afzal Hossain <afzal.csedu@gmail.com>
2017-03-25 18:46:16 +0600

    after gulp

--- theme.js
+++ theme.js
@@ -8270,7 +8270,6 @@ Picker.extend( 'pickadate', DatePicker )
 
 jQuery(document).ready(function () {
     jQuery('ul#primary-menu > li.menu-item-has-children').click(function (e) {
-        console.log(jQuery(this).find('ul').first().css('opacity'));
         if (jQuery(this).find('ul').first().css('opacity') < 1) {
             e.preventDefault();
         }
