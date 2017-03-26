9c280bf05bc71235acadf3ada4b13b32d8bdba71
Afzal Hossain <afzal.csedu@gmail.com>
2017-03-26 19:47:24 +0600

    side nav

--- header.php
+++ header.php
@@ -12,11 +12,11 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
 
-<?php wp_head(); ?>
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
@@ -25,9 +25,22 @@
 		<nav id="site-navigation" class="grey lighten-3 main-navigationgrey-text text-darken-2" role="navigation">
 			<div class="nav-wrapper container">
 				<a href="/" id="logo-container" class="brand-logo">Logo Here</a>
-			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu', 'menu_class' => 'right hide-on-med-and-down' ) ); ?>
+				<?php wp_nav_menu( array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+					'menu_class'     => 'right hide-on-med-and-down'
+				) ); ?>
+
+				<?php wp_nav_menu( array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'mobile-menu',
+					'menu_class'     => 'side-nav'
+				) ); ?>
+				<a href="#" data-activates="mobile-menu" class="button-collapse"><i class="material-icons">menu</i></a>
 			</div>
+
 		</nav><!-- #site-navigation -->
+
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content container">
--- js/menu.js
+++ js/menu.js
@@ -3,5 +3,17 @@ jQuery(document).ready(function () {
         if (jQuery(this).find('ul').first().css('opacity') < 1) {
             e.preventDefault();
         }
-    })
+    });
+
+    jQuery('.button-collapse').sideNav({
+            menuWidth: 300, // Default is 300
+            edge: 'left', // Choose the horizontal origin
+            closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+            draggable: true // Choose whether you can drag to open on touch screens
+        }
+    );
+    if (jQuery('#wpadminbar').length) {
+        jQuery('#mobile-menu').css('padding-top', '46px');
+    }
+
 });
