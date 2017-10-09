2c086afa0bfea2d08921b0e5cdabf285492cff88
Afzal Hossain <afzal.csedu@gmail.com>
2017-10-09 19:32:26 +0600

    google adsense

--- header.php
+++ header.php
@@ -15,6 +15,13 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<script>
+	  (adsbygoogle = window.adsbygoogle || []).push({
+	    google_ad_client: "ca-pub-4204843715334345",
+	    enable_page_level_ads: true
+	  });
+	</script>
 
 	<?php wp_head(); ?>
 </head>
